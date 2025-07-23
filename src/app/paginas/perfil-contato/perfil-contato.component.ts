bc0a6b2916f206897192debed76e709f2713f4f0
Guilherme França Fernandes <guilfranca27@gmail.com>
2025-07-23 17:23:42 -0300

    Aula 3 - Finalizada

--- src/app/paginas/perfil-contato/perfil-contato.component.ts
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
 import { Contato } from '../../componentes/contato/contato';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ContatoService } from '../../services/contato.service';
 
 @Component({
   selector: 'app-perfil-contato',
@@ -17,15 +18,41 @@ import { RouterLink } from '@angular/router';
   templateUrl: './perfil-contato.component.html',
   styleUrl: './perfil-contato.component.css'
 })
-export class PerfilContatoComponent {
+export class PerfilContatoComponent implements OnInit{
 
   contato: Contato = {
     id: 0,
-    nome: 'dev',
-    telefone: '123123123',
-    email : 'dev@email.com',
-    aniversario: '12/10/1990',
-    redes: 'dev.com'
+    nome: '',
+    telefone: '',
+    email : '',
+    aniversario: '',
+    redes: ''
+  }
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private contatoService: ContatoService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    // snapshot captura instântantea das informações do contato naquele exato momento
+    // paramMap para pegar os parâmetros da rota
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    // O get do activatedRoute está nos retornando uma string, por conta disso iremos precisar converter o id para que o método buscarPorId receba a mesma como number
+    // E ainda nos dará um erro, pois o mesmo pode ser null, nesse caso podemos utilizar um ! (operador de acerção não nula) depois do id, ou então utilzar um if para só fazer isso caso o id exista.
+    if(id) {
+      this.contatoService.buscarPorId(parseInt(id)).subscribe((contato) => {
+        this.contato = contato
+      })
+    }
+  }
+
+  excluir() {
+    if (this.contato.id) {
+      this.contatoService.excluirContato(this.contato.id).subscribe(() => {});
+      this.router.navigateByUrl('/lista-contatos');
+    }
   }
 
 }
--- src/app/services/contato.service.ts
+++ src/app/services/contato.service.ts
@@ -22,8 +22,34 @@ export class ContatoService {
     return this.http.get<Contato[]>(this.API);
   }
 
-  salvarContato(contato: Contato) {
+  salvarContato(contato: Contato): Observable<Contato> {
     // O método post é utilizado para a inserção de contatos
     return this.http.post<Contato>(this.API, contato)
   }
+
+  buscarPorId(id: number): Observable<Contato> {
+    // Concatenação do caminho da API mais o id enviado pelo método
+    const url = `${this.API}/${id}`
+    return this.http.get<Contato>(url)
+  }
+
+  excluirContato(id: number): Observable<Contato> {
+    const url = `${this.API}/${id}`
+    return this.http.delete<Contato>(url)
+  }
+
+  editarContato(contato: Contato): Observable<Contato> {
+    const url = `${this.API}/${contato.id}`
+    // put é o método http para realizar edições no contato
+    return this.http.put<Contato>(url, contato);
+  }
+
+  editarOuSalvarContato(contato: Contato): Observable<Contato> {
+    if (contato.id) {
+      return this.editarContato(contato);
+    } else {
+      return this.salvarContato(contato);
+    }
+  }
+
 }
